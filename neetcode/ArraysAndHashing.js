b84044e4be1dbf3af6920338ae335810f5f44488
Coral Dabarera Edelson <corald@gmail.com>
2023-03-27 16:54:50 -0700

    working on product

--- neetcode/ArraysAndHashing.js
+++ neetcode/ArraysAndHashing.js
@@ -400,7 +400,8 @@ const topK3 = (nums, k) => {
 
  /* <------------------------- Product of Array except self ----------------------> */
 
-//  Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//  Given an integer array nums, return an array answer such that 
+// answer[i] is equal to the product of all the elements of nums except nums[i].
 //  The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 //  You must write an algorithm that runs in O(n) time and without using the division operation.
  
@@ -425,8 +426,12 @@ const xOfArray = (nums) => {
   let product = 1
 
   for ( let i = 0; i < nums.length; i ++) {
-    product *= nums[i]
-    console.log(product)
+    let nums = nums.slice(i+1)
+    let curr = 1
+    let ans
+    for ( let j = 0; j < nums.length; j ++){
+      curr*= nums[j]
+    }
   }
 
   for ( let j = 0; j < nums.length; j++){
