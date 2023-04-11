907b4c9ec7940bfd86367b200bae442f17dd402b
Coral Dabarera Edelson <corald@gmail.com>
2023-04-11 11:59:46 -0700

    stack

--- neetcode/TwoPointer.js
+++ neetcode/TwoPointer.js
@@ -59,6 +59,48 @@ function isPalindrome (s) {
 
 
 
-console.log(isPalindrome("A man, a plan, a canal: Panama"))
-console.log(isPalindrome("Hannah"))
-console.log(isPalindrome("race a car"))
+// console.log(isPalindrome("A man, a plan, a canal: Panama"))
+// console.log(isPalindrome("Hannah"))
+// console.log(isPalindrome("race a car"))
+
+
+ /* <------------------------- 3Sum ----------------------> */
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+// Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+const ThreeSum = (arr) => {
+
+  // save sums, if hit a number that is the negative of that number, put all those
+  // numbers in an array
+
+  const returner = [];
+  
+
+
+}
+
